7e3b33cc2ef00065fff35506a2812656f0937138
chaotive <leonardo.montenegro@chaotive.cl>
2017-07-04 17:00:58 -0400

    Project init

---
+++ Assets/Scripts/GoScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoScene : MonoBehaviour {
+    public string sceneName = "anScene";
+    
+    public void goScene() {
+        SceneManager.LoadScene(sceneName);
+    }
+}
